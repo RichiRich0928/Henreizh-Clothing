4f1e8b5a78af7fceb9efc0061b259f2e30ca7f7d
pxpxyxx <72678670+skygorithm@users.noreply.github.com>
2025-10-02 19:20:03 +0800

    UPDATE

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import '../../components/styles/utilities/Navbar.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <div className="header-main">
+        <div className="header-left">
+            <img />
+            <button>Home</button>
+            <button>Shop</button>
+            <button>Collections</button>
+            <button>About Us</button>
+            <button>Contact</button>
+        </div>
+        <div className="header-right">      
+
+        </div>
+    </div>
+    </>
   )
 }
 
