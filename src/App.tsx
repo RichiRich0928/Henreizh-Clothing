78b36cc539551d7a42f9aec304e216f738f8c906
pxpxyxx <72678670+skygorithm@users.noreply.github.com>
2025-10-05 22:06:29 +0800

    LINKED CSS IN INDEX.HTML, ADDED ROUTE FOR PROFILE

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Collections from "./components/collections/Collections";
 import Contact from "./components/contact/Contact";
 import Shop from "./components/shoppage/Shop";
 import Signup from "./components/signup/Signup";
+import Profile from "./components/profilepage/Profile";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Route path="/contact" element={<Contact />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/profile" element={<Profile/>} />
     </Routes>
   );
 }
---
+++ src/components/profilepage/Profile.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Layout from '../layouts/Layout'
+import "../styles/Profile.css"
+
+const Profile = () => {
+  return (
+    <>
+    <Layout>
+        <div className='header-main'>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </Layout>
+    </>
+  )
+}
+
+export default Profile
