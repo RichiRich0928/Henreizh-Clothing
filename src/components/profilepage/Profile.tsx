3ad506a50a5908e828804ca52fc6b2029af708dd
Tristan814 <tristanaquino814@gmail.com>
2025-10-08 00:11:54 +0800

    Complete Profile

--- src/components/profilepage/Profile.tsx
+++ src/components/profilepage/Profile.tsx
@@ -3,23 +3,40 @@ import "../styles/Profile.css";
 import profileImg from "../../assets/pants.png";
 import AppHeader from "../layouts/AppHeader";
 import Footer from "../layouts/Footer";
+import headset from "../../assets/headset.png";
+import questionmark from "../../assets/questionmark.png";
+import address from "../../assets/address.png";
+import creditcard from "../../assets/creditcard.png";
+import edit from "../../assets/edit.png";
+import emailletter from "../../assets/emailletter.png";
+import lock from "../../assets/lock.png";
+import drag from "../../assets/drag.png";
+import profile from "../../assets/profile.png";
+import getFontSizes from "antd/es/theme/themes/shared/genFontSizes";
+
+
 const Profile: React.FC = () => {
   return (
     <>
     <AppHeader />
-
       <div className="profile-header">
         <div className="profile-column">
         <img src={profileImg} alt="Profile" className="profile-image" />
-        <button className="edit-profile-btn">Edit Profile</button>
+        <div className="title-edit">
+          
+<button className="edit-profile-btn">Edit Profile</button>
+<img src={edit} className="icon-edit" alt="edit" />
+        </div>
+        
         </div>
         <div className="profile-header-text">
           <h2>WELCOME</h2>
           <p>Nathan</p>
         </div>
+        {/* <img src={lizard} alt="logo" className="lizard-image" /> */}
       </div>
 
-    
+    <div className="container">
       <div className="orders-section">
         <h2>ORDERS & RETURNS</h2>
         <div className="orders-box">
@@ -44,55 +61,83 @@ const Profile: React.FC = () => {
         {/* Info Grid */}
         <div className="info-grid">
           <div className="info-card">
-            <h4>INFORMATION</h4>
+            <div className="info-card-header">
+              <div className="info-title">
+                <img src={profile} className="icon-left" alt="info" />
+                <h2>INFORMATION</h2>
+              </div>
+              <img src={edit} className="icon-edit" alt="edit" />
+            </div>
             <p>Nathan Aruta</p>
             <p>nathanaruta@gmail.com</p>
           </div>
 
           <div className="info-card">
-            <h4>LOGIN & PASSWORD</h4>
+            <div className="info-card-header">
+              <div className="info-title">
+                <img src={lock} className="icon-left" alt="lock" />
+                <h2>LOGIN & PASSWORD</h2>
+              </div>
+              <img src={edit} className="icon-edit" alt="edit" />
+            </div>
             <p>Login: nathanaruta@gmail.com</p>
             <p>Password: ********</p>
           </div>
 
-          <div className="info-card newsletter-card">
-            <div>
-              <h4>NEWSLETTER</h4>
-              <p>Login: nathanaruta@gmail.com</p>
+          <div className="info-card">
+            <div className="info-card-header">
+              <div className="info-title">
+                <img src={emailletter} className="icon-left" alt="mail" />
+                <h2>NEWSLETTER</h2>
+              </div>
+          
             </div>
-            <input type="checkbox" defaultChecked />
+            <p>Login: nathanaruta@gmail.com</p>
+            <img src={drag} className="icon-drag" alt="toggle" />
           </div>
+     
         </div>
 
         {/* Address and Credit */}
-        <div className="info-grid">
+        <div className="info-grid2">
           <div className="dashed-card">
-            <h4>NO ADDRESS INPUT</h4>
+            <img src = {address} className="small-icons"/>
+            <h2>NO ADDRESS INPUT</h2>
             <p>Find Your Default Address Here</p>
             <button className="outline-btn">COMPLETE YOUR ADDRESS</button>
           </div>
 
           <div className="dashed-card">
-            <h4>NO CREDIT CARD ADDED</h4>
+            <img src = {creditcard} className="small-icons"/>
+            <h2>NO CREDIT CARD ADDED</h2>
             <p>Save your default payment method during checkout</p>
           </div>
         </div>
 
         {/* Contact and FAQ */}
-        <div className="info-grid">
+        <div className="info-grid3">
+          
           <div className="gray-card">
-            <h4>CONTACT OUR CUSTOMER SERVICE</h4>
+            <div className="gray-card-title">
+           <img src = {headset} className="small-icons"/>
+            
+            <h3>CONTACT OUR CUSTOMER SERVICE</h3>
+            </div>
             <p className="contact-number">8-7000</p>
           </div>
 
           <div className="gray-card">
-            <h4>FAQ</h4>
-            <p>
+            <div className="gray-card-title">
+            <img src = {questionmark} className="small-icons"/>
+            <h2>FAQ</h2>
+            </div>
+            <p className="FAQ">
               You might find the answer you're looking for in our frequently
               asked questions.
             </p>
           </div>
         </div>
+      </div>
       </div>
     <Footer />
     </>
